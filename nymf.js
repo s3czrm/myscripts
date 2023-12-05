b8d6875b7ccc9d715518afdd6f3a7f70d8761409
爻 <92191927+s3czrm@users.noreply.github.com>
2023-12-06 01:27:26 +0800

    更新 nymf.js

--- nymf.js
+++ nymf.js
@@ -8,20 +8,7 @@ hostname = nymfapp.com
 
 */
 
+let string = $response.body;
+string = string.replace(/"pro":true/g, '"pro":false');
+$done({body: string});
 
-let Premium = $response.body;
-var modified = JSON.parse(Premium);
-
-function recursiveReplace(obj) {
-  for (var key in obj) {
-    if (typeof obj[key] === 'object') {
-      recursiveReplace(obj[key]); // 递归处理对象或数组
-    } else if (key === 'pro' && obj[key] === true) {
-      obj[key] = false; // 替换匹配到的值
-    }
-  }
-}
-
-recursiveReplace(modified);
-
-$done({ body: JSON.stringify(modified) });
