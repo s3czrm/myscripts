5c52c94c76e42ea6433ecb6b8fbc0846647626f2
爻 <92191927+s3czrm@users.noreply.github.com>
2023-12-26 15:12:35 +0800

    更新 yinhu.js

--- yinhu.js
+++ yinhu.js
@@ -9,6 +9,8 @@ var body = $response.body;
 var obj = JSON.parse(body); 
 // 修改数据字段
 obj.data.vip_type = "3";
+obj.data.type = "3";
+obj.data.pay_switch = "3";
 
 // 重写响应体
 $done({ body: JSON.stringify(obj) });
