3cc0add3b08199455563889ada277619adb5f928
爻 <92191927+s3czrm@users.noreply.github.com>
2023-12-26 15:08:46 +0800

    更新 yinhu.js

---
+++ yinhu.js
@@ -0,0 +1,14 @@
+/*
+[riwrute_local]
+https:\/\/foxapi\.zduhxck\.com\/vpn\/user_info url script-response-body https://raw.githubusercontent.com/s3czrm/myscripts/main/yinhu.js
+[mitm] 
+hostname=foxapi.zduhxck.com
+
+***********************************/
+var body = $response.body; 
+var obj = JSON.parse(body); 
+// 修改数据字段
+obj.data.vip_type = "3";
+
+// 重写响应体
+$done({ body: JSON.stringify(obj) });
