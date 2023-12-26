5d19b377425bbc507a41c5383b43453e5e9653e6
爻 <92191927+s3czrm@users.noreply.github.com>
2023-12-26 13:07:50 +0800

    更新 ml.js

---
+++ ml.js
@@ -0,0 +1,10 @@
+[mitm] 
+hostname=*.mlinkapp.cc
+
+***********************************/
+var body = $response.body; 
+var obj = JSON.parse(body); 
+// 修改数据字段
+obj.data.level = "3";
+// 重写响应体
+$done({ body: JSON.stringify(obj) });
