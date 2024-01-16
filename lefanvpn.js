7b4daac4802c855484b82f923a6e2eb17655b63c
爻 <92191927+s3czrm@users.noreply.github.com>
2024-01-16 21:31:40 +0800

    更新 lefanvpn.js

--- lefanvpn.js
+++ lefanvpn.js
@@ -42,7 +42,7 @@ var egol = JSON.parse($response.body);
     "inviteUrl" : "https://www.lofun.lat/app.html?channel=GW&channel_code=&invite_code=XCC077RR",
     "totalTransfer" : 214748364800,
     "expiredDate" : "2999-09-09",
-    "vip" : 2,
+    "vip" : "1",
     "channel_qq_count" : 1,
     "cardActive" : true,
     "username" : "User_8638452083162834789"
