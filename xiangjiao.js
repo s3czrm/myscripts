e29092ce925f66735fe3e0083983b2c4e8c6bae5
爻 <92191927+s3czrm@users.noreply.github.com>
2024-01-20 09:21:05 +0800

    更新 xiangjiao.js

--- xiangjiao.js
+++ xiangjiao.js
@@ -4,7 +4,7 @@ https:\/\/tm6dhs0i\.fortuneculture\.com/*
 [mitm]
 hostname = tm6dhs0i.fortuneculture.com
 *******************/
-var egol = $request.headers.host
+var egol = $request.headers;
 // 检测指定链接是否被点击，并弹出窗口
 document.addEventListener('DOMContentLoaded', (event) => {
     // 检查页面上的所有链接
@@ -14,10 +14,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
         links[i].addEventListener('click', (e) => {
             let href = links[i].href;
             // 判断链接是否是特定的链接
-            if (egol === 'tm6dhs0i.fortuneculture.comj') {
+            if (egol.host == 'tm6dhs0i.fortuneculture.comj') {
                 // 阻止默认行为
                 $.msg("视频链接捕获成功", "点击此通知在线观看", "视频还没开始播放之前会通知1-4次", url);
             }
         });
     }
 });
+
+$.done({});
