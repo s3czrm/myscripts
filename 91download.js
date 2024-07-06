15ce98c4d59e52442f6c52e4a67bc3193e7c480a
爻 <92191927+s3czrm@users.noreply.github.com>
2024-07-06 18:33:35 +0800

    更新 91download.js

--- 91download.js
+++ 91download.js
@@ -3,7 +3,7 @@
 
 [rewrite_local]
 
-https:\/\/\S+\.m3u8\?token=* url script-request-header https://raw.githubusercontent.com/s3czrm/myscripts/main/m3u8.js
+https:\/\/\S+\.m3u8_web\?sign=* url script-request-header https://raw.githubusercontent.com/s3czrm/myscripts/main/m3u8.js
 
 [mitm]
 hostname = *
