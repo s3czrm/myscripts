d2071ea00b3830a5ec4e854e57ab39d7e173c94d
爻 <92191927+s3czrm@users.noreply.github.com>
2025-02-09 05:17:04 +0800

    更新 one.js

--- one.js
+++ one.js
@@ -1,5 +1,5 @@
 // 定义匹配规则
-const urlPattern = /^https?:\/\/akmaivideo\.cdchuangao\.com.*\.m3u8/;
+const urlPattern = /^https?:\/\/akmaivideo\.cdchuangao\.com.*\.mp4/;
 
 // 监听请求
 $done({
@@ -11,7 +11,7 @@ $done({
 // 检查请求的URL是否匹配
 if (urlPattern.test($request.url)) {
     // 弹出对话框
-    $notification.post("匹配到m3u8链接", "点击打开链接", $request.url, {
+    $notification.post("匹配到mp4链接", "点击打开链接", $request.url, {
         "url": $request.url
     });
 }
