a3f331701c3a491aac62df209b1f9adfb8a9a767
爻 <92191927+s3czrm@users.noreply.github.com>
2025-03-12 01:07:47 +0800

    更新 m3u8888.js

--- m3u8888.js
+++ m3u8888.js
@@ -8,7 +8,7 @@ let url = $request.url;
         const notify = $.getdata("mp4");
         //console.log("Saved notify:", notify);
         if (!notify || notify != url) {
-            $.setdata(url, "mp4");
+            $.setdata(url, "m3u8");
             $.msg("视频链接捕获成功", "点击此通知在线观看", "视频还没开始播放之前会通知1-4次", url);
         }
     } catch (e) {
