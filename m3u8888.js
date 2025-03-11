01c7ccb0be96bab28205ccfc83d4083d39e607ac
爻 <92191927+s3czrm@users.noreply.github.com>
2025-03-12 01:06:00 +0800

    更新 m3u8888.js

---
+++ m3u8888.js
@@ -0,0 +1,21 @@
+const $ = new Env("GOGOGOGO");
+let url = $request.url;
+// yuheng基础上更改保留auth_key
+
+// X-Playback-Session-Id头部
+
+    try {
+        const notify = $.getdata("mp4");
+        //console.log("Saved notify:", notify);
+        if (!notify || notify != url) {
+            $.setdata(url, "mp4");
+            $.msg("视频链接捕获成功", "点击此通知在线观看", "视频还没开始播放之前会通知1-4次", url);
+        }
+    } catch (e) {
+        console.error("An error occurred:", e);
+    }
+
+
+$.done({});
+
+
